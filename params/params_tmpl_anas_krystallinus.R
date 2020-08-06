5236d597044bd647bd2d2b95e369f69a078cc18b
Anna Krystalli <annakrystalli@googlemail.com>
2020-08-06 17:52:06 +0100

    Close quotes

--- params/params_tmpl_anas_krystallinus.R
+++ params/params_tmpl_anas_krystallinus.R
@@ -5,11 +5,11 @@
 
 # sigma2: 0 < value < 5
 sig2 <- 2
-    
-# e.g. "anas_krystallinus"    
-species.name <- "anas_krystallinus
-    
-# e.g. "red"    
+
+# e.g. "anas_krystallinus"
+species.name <- "anas_krystallinus"
+
+# e.g. "red"
 color <- "purple"
-    
+
 # tip: pick a color at using rstudio colour picker. Addins > Colour Picker
