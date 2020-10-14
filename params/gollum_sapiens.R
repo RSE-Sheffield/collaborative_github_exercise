7562fb5ff07f41a81bcd07a2b26416eacc011cbe
ioannouE <eioannou1@sheffield.ac.uk>
2020-10-14 12:28:23 +0100

    added parameters for gollum_sapiens

---
+++ params/gollum_sapiens.R
@@ -0,0 +1,12 @@
+# input parameters
+
+# sigma2: 0 < value < 5
+sig2 <- 4.6
+
+# e.g. "anas_krystallinus"
+species.name <- "gollum_sapiens"
+
+# e.g. "red"
+color <- "darkorchid4"
+
+# tip: pick a color at using rstudio colour picker. Addins > Colour Picker
