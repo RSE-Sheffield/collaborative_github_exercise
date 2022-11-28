e0db1d2f7056285c634d087be6265f0882b26d24
Guilhem Collier <g.j.collier@sheffield.ac.uk>
2022-11-28 12:06:28 +0000

    add parameters for params_tmpl_GC

---
+++ params/params_tmpl_GC.R
@@ -0,0 +1,13 @@
+# ________________________________________________
+# input parameters
+
+# sigma2: 0 < value < 5
+sig2 <- 2.8
+    
+# e.g. "anas_krystallinus"    
+species.name <- "params_tmpl_GC"
+    
+# e.g. "red"    
+color <- "blue"
+    
+# For list of colours available in R, see http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
