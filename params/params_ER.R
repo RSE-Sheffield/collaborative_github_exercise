03e734442ad3286dc24f73d9aaa83db8da63fa0d
Edward-Rawson <erawson2@sheffield.ac.uk>
2024-03-18 11:44:43 +0000

    Added new params file

---
+++ params/params_ER.R
@@ -0,0 +1,12 @@
+# input parameters
+
+# sigma2: 0 < value < 5
+sig2 <- 2.678478
+    
+# e.g. "anas_krystallinus"    
+species.name <- "Big bird"
+    
+# e.g. "red"    
+color <- "orange1"
+    
+# For list of colours available in R, see http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
