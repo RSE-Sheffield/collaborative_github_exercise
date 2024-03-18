b0d4a8e2c245424b675d5ba01cf69c3a5d50287c
anthonysimons <a.j.simons@sheffield.ac.uk>
2024-03-18 11:45:32 +0000

    Adding Corvus Simonsae species in params_Tony.R

---
+++ params/params_Tony.R
@@ -0,0 +1,15 @@
+# !! YOU MAY EDIT THIS FILE !!
+# ** make a copy and edit that instead **
+# ________________________________________________
+# input parameters
+
+# sigma2: 0 < value < 5
+sig2 <- 4
+    
+# e.g. "anas_krystallinus"    
+species.name <- "Corvus Simonsae"
+    
+# e.g. "red"    
+color <- "black"
+    
+# For list of colours available in R, see http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
