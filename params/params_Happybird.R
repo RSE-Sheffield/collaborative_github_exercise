925dae7b03d908764a69f608b4c8cb9e29b0ba57
Juntong Lai <94724539+LaiDream@users.noreply.github.com>
2025-01-20 11:41:27 +0000

    Update params_Happybird.R
    new findings

--- params/params_Happybird.R
+++ params/params_Happybird.R
@@ -4,12 +4,12 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 2.5
     
 # e.g. "anas_krystallinus"    
-species.name <- " "
+species.name <- "happy bird "
     
 # e.g. "red"    
-color <- " "
+color <- " blue"
     
 # For list of colours available in R, see http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
