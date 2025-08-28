426d0c744a77bb5b072979573eb98ebeb2c554a1
Johnny <jcmitchell98@outlook.com>
2025-08-28 14:55:30 +0100

    Add Parameters
    Added parameters for a new bird, as described in the cloning and collaborating section of the tutorial.

---
+++ params/Johnnys_bird.R
@@ -0,0 +1,12 @@
+# input parameters
+
+# sigma2: 0 < value < 5
+sig2 <- 3
+    
+# e.g. "anas_krystallinus"    
+species.name <- "john_bongollinus"
+    
+# e.g. "red"    
+color <- "#55FF55"
+    
+# For list of colours available in R, see http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
