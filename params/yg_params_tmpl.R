fa85561d7392a2154a5b98fb6eae9dfcc160948a
yiyang-gao <y.gao@sheffield.ac.uk>
2025-10-28 10:55:20 +0000

    Input parameters

---
+++ params/yg_params_tmpl.R
@@ -0,0 +1,13 @@
+
+# input parameters
+
+# sigma2: 0 < value < 5
+sig2 <- 3
+    
+# e.g. "anas_krystallinus"    
+species.name <- "Yiyang's Bird"
+    
+# e.g. "red"    
+color <- "darkviolet"
+    
+# For list of colours available in R, see http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
