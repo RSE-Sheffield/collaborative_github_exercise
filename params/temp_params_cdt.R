17e198537a00290345430e7069878453c8e621c4
ariameetgit <queshumin@outlook.com>
2026-04-22 11:54:31 +0100

    exercise request

---
+++ params/temp_params_cdt.R
@@ -0,0 +1,14 @@
+
+# input parameters
+
+# sigma2: 0 < value < 5
+sig2 <- 2
+
+# e.g. "anas_krystallinus"
+species.name <- "cdt"
+
+# e.g. "red"
+color <- "azure"
+
+# For list of colours available in R, see
+# https://github.com/RSE-Sheffield/collaborative_github_exercise/tree/master/assets/Rcolor.pdf
