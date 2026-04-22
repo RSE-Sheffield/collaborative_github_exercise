46689ed82b2755911e7e9e13e04fa2f1f1c8a017
AKhan <ahmed+github@khanhome.net>
2026-04-22 11:27:55 +0100

    Added in the rest of params

--- params/params_tmpl.R
+++ params/params_tmpl.R
@@ -4,10 +4,10 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 3
 
 # e.g. "anas_krystallinus"
-species.name <- " "
+species.name <- " anas_krystallinus"
 
 # e.g. "red"
 color <- " Blue"
