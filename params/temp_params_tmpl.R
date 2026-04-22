4337776125ceb01afb61c468a90b572af0716eea
Sean Cragg <s.k.cragg@gmail.com>
2026-04-22 11:25:41 +0100

    added new param temp file

---
+++ params/temp_params_tmpl.R
@@ -0,0 +1,14 @@
+
+# input parameters
+
+# sigma2: 0 < value < 5
+sig2 <- 4.1
+
+# e.g. "anas_krystallinus"
+species.name <- "corvus_corbyn"
+
+# e.g. "red"
+color <- "midnightblue"
+
+# For list of colours available in R, see
+# https://github.com/RSE-Sheffield/collaborative_github_exercise/tree/master/assets/Rcolor.pdf
